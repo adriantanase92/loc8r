181759c18d99169e297a672e3ae2e1e8730d8db6
Adrian Tanase <tanase.adrian92@gmail.com>
2019-10-16 21:11:52 +0300

    wip

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-/* GET homepage. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
